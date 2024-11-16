73564c51fb58fea97e5b7a3183b175fcb2bd0e22
avi <avineshwarthegreat@gmail.com>
2024-11-16 15:52:57 +0530

    frontend added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend's URL
+    origin: "https://aviloginsysytem.netlify.app/", // Replace with your frontend's URL
     credentials: true,
   })
 );
