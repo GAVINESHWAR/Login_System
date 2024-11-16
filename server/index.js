f596daaea4bac0cbf905eaf26f2117626feb0189
avi <avineshwarthegreat@gmail.com>
2024-11-16 16:53:27 +0530

    styles

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const CarModel = require("./model/Car"); // Import the Car model
 
 dotenv.config();
 const app = express();
+const port =  process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
